aec76434700d835a03218ac30bf3da79a12faa4b
ПК <@DESKTOP-59BIPOH>
2022-01-26 17:36:30 +0200

    Добавьте файлы проекта.

---
+++ ReadMe.txt
@@ -0,0 +1,9 @@
+Implement a test application logic.
+ Find a sample C# solution in "RomanMath.zip" archive and all test application requirenments in "TODO.txt" file.
+ Pay attention to extra comments inside the project.
+Publish your implementation to GitHub and send us back a link.
+
+Реалізуйте логіку тестової програми.
+ Знайдіть зразок рішення C# в архіві "RomanMath.zip", а всі вимоги до тестової програми - у файлі "TODO.txt".
+ Зверніть увагу на додаткові коментарі всередині проекту.
+Опублікуйте свою реалізацію на GitHub і надішліть нам посилання.
---
+++ RomanMath.Impl/Service.cs
@@ -0,0 +1,92 @@
+﻿using MathNet.Numerics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RomanMath.Impl
+{
+	public static class Service
+	{
+		enum Roman 
+        {
+			M = 1000,
+			D = 500,
+			C = 100,
+			L = 50,
+			X = 10,
+			V = 5,
+			I = 1
+		};
+		/// <summary>
+		 /// See TODO.txt file for task details.
+		 /// Do not change contracts: input and output arguments, method name and access modifiers
+		 /// </summary>
+		 /// <param name="expression"></param>
+		 /// <returns></returns>
+
+		public static int Evaluate(string expression)
+		{
+			
+			Validate(expression);
+
+			Roman roman = new Roman();
+
+			int values = 0;
+
+			//Нужно както сравнить Enum с каждым символом в выражении expression , при этом придав заменив их значениеями 
+			           			
+			if (expression.Contains(Convert.ToString(roman)))
+			{
+				for (int i = 0; i < expression.Length; i++)
+				{
+					// values = ...
+				}
+
+			}
+			return values;
+			
+		}
+
+        private static void Validate(string expression)
+        {
+			Exception ex = new Exception("the input expression is invalid");
+
+			
+			if (expression.IsNullOrEmpty())
+            {
+				throw ex;
+            }
+			else if (expression.Contains("/"))
+            {
+				throw ex;
+			}
+			else if (expression.Contains("%"))
+			{
+				throw ex;
+			}
+			
+		}
+
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> enumerable)
+		{
+			if (enumerable == null)
+			{
+				return true;
+			}
+			/* If this is a list, use the Count property for efficiency. 
+			 * The Count property is O(1) while IEnumerable.Count() is O(N). */
+			var collection = enumerable as ICollection<T>;
+			if (collection != null)
+			{
+				return collection.Count < 1;
+			}
+			return !enumerable.Any();
+		}
+
+
+	}
+	
+	
+
+
+}
